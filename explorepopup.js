99eadbba3dbfa1054abbea50b619f6dd23d44c95
nelsosp <124758584+nelsosp@users.noreply.github.com>
2023-07-29 01:34:44 -0400

    Added detail popup section and images/features

--- explorepopup.js
+++ explorepopup.js
@@ -1,4 +1,8 @@
 // Function to activate popup window when Talk to Realtor is clicked
-function togglePopup() {
+function toggleRealtorPopup() {
     document.getElementById("popup-1").classList.toggle("active");
+}
+
+function toggleDetailPopup() {
+    document.getElementById("popup-2").classList.toggle("active");
 }
